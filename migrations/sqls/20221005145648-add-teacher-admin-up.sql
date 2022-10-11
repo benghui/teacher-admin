649eb8125d0982894249dda2ebd9eca3cd7cd0b0
benghui <benghui@gmail.com>
2022-10-11 16:11:11 +0800

    Fix separate out seed data for db-migrate

--- migrations/sqls/20221005145648-add-teacher-admin-up.sql
+++ migrations/sqls/20221005145648-add-teacher-admin-up.sql
@@ -22,24 +22,4 @@ CREATE TABLE register (
 	REFERENCES students (id) ON DELETE CASCADE
 );
 
-CREATE INDEX fk_to_students ON register (student_id);
-
-INSERT INTO teachers (email) VALUES("teacherken@gmail.com");
-INSERT INTO teachers (email) VALUES("teacherjoe@gmail.com");
-INSERT INTO teachers (email) VALUES("teachersally@gmail.com");
-
-INSERT INTO students (email) VALUES("studentjon@gmail.com");
-INSERT INTO students (email) VALUES("studenthon@gmail.com");
-INSERT INTO students (email) VALUES("studentpete@gmail.com");
-INSERT INTO students (email) VALUES("studentmary@gmail.com");
-INSERT INTO students (email) VALUES("studentbob@gmail.com");
-INSERT INTO students (email) VALUES("studentmiche@gmail.com");
-INSERT INTO students (email) VALUES("studentagnes@gmail.com");
-
-INSERT INTO register (teacher_id, student_id) VALUES(1, 1);
-INSERT INTO register (teacher_id, student_id) VALUES(1, 3);
-INSERT INTO register (teacher_id, student_id) VALUES(1, 4);
-INSERT INTO register (teacher_id, student_id) VALUES(2, 1);
-INSERT INTO register (teacher_id, student_id) VALUES(2, 2);
-INSERT INTO register (teacher_id, student_id) VALUES(2, 7);
-INSERT INTO register (teacher_id, student_id) VALUES(3, 1);
+CREATE INDEX fk_to_students ON register (student_id)
---
+++ migrations/sqls/20221011080709-seed-up.sql
@@ -0,0 +1,19 @@
+INSERT INTO teachers (email) VALUES("teacherken@gmail.com");
+INSERT INTO teachers (email) VALUES("teacherjoe@gmail.com");
+INSERT INTO teachers (email) VALUES("teachersally@gmail.com");
+
+INSERT INTO students (email) VALUES("studentjon@gmail.com");
+INSERT INTO students (email) VALUES("studenthon@gmail.com");
+INSERT INTO students (email) VALUES("studentpete@gmail.com");
+INSERT INTO students (email) VALUES("studentmary@gmail.com");
+INSERT INTO students (email) VALUES("studentbob@gmail.com");
+INSERT INTO students (email) VALUES("studentmiche@gmail.com");
+INSERT INTO students (email) VALUES("studentagnes@gmail.com");
+
+INSERT INTO register (teacher_id, student_id) VALUES(1, 1);
+INSERT INTO register (teacher_id, student_id) VALUES(1, 3);
+INSERT INTO register (teacher_id, student_id) VALUES(1, 4);
+INSERT INTO register (teacher_id, student_id) VALUES(2, 1);
+INSERT INTO register (teacher_id, student_id) VALUES(2, 2);
+INSERT INTO register (teacher_id, student_id) VALUES(2, 7);
+INSERT INTO register (teacher_id, student_id) VALUES(3, 1);
